b59de4313abcb41c656947b8291c657ad0c6ca86
baroman4i <baroman4i@mail.ru>
2021-11-21 19:24:47 +0300

    react

--- 2.js
+++
@@ -1,3 +0,0 @@
-const sayHello = (name) => name == 'Mark' ? `Hi, ${name}!` : `Hello, ${name}!`
-console.log(sayHello('Oleg'))
-console.log(sayHello('Mark'))
--- 3.js
+++
@@ -1,2 +0,0 @@
-const filterStrings = (strArr, count) => strArr.filter(word => word.length <= count)
-console.log(filterStrings(['a', 'ab', 'abc'], 2))
